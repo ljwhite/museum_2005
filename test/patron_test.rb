db8e8f2d9e0cb853ce6979d787e1f63f37c87785
Lito White <leland.white@gmail.com>
2020-05-27 09:17:26 -0600

    Iteration 1 complete

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -13,4 +13,19 @@ class PatronTest < Minitest::Test
     assert_instance_of Patron, @patron_1
 
   end
+
+  def test_it_has_readable_attributes
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+    assert_equal [], @patron_1.interests
+
+  end
+
+  def test_patron_can_add_interests
+    assert_equal [], @patron_1.interests
+    @patron_1.add_interests("Dead Sea Scrolls")
+    @patron_1.add_interests("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
+
+  end
 end
